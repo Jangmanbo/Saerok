9015efed0b81a8c14cf960188b5cda5780296932
Jangmanbo <wkddpdnjs99@gmail.com>
2021-01-31 04:03:18 +0900

    revise post, mypage xml

--- app/src/main/java/com/example/Saerok/PostFragment.java
+++ app/src/main/java/com/example/Saerok/PostFragment.java
@@ -1,5 +1,7 @@
 package com.example.Saerok;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -7,6 +9,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,14 +19,34 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+
+import static android.app.Activity.RESULT_OK;
+
 public class PostFragment extends Fragment {
+    //private static final int GET_IMAGE_FOR_IMAGELAYOUT = 205;
+
     androidx.appcompat.widget.Toolbar toolbar;
+    ImageButton galleryButton;
+    //LinearLayout imageLayout;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.activity_post, container, false);
         toolbar=view.findViewById(R.id.toolbar);
+        galleryButton = view.findViewById(R.id.galleryButton);
+
+        /*
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_IMAGE_FOR_IMAGELAYOUT);
+            }
+        });
+        */
 
         return view;
     }
@@ -42,7 +67,38 @@ public class PostFragment extends Fragment {
                 }
             }
         });
+
     }
+
+
+    /*@Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Uri selectedImageUri;
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && data != null && data.getData() != null) {
+            switch(requestCode) {
+                case GET_IMAGE_FOR_IMAGELAYOUT:
+                    selectedImageUri = data.getData();
+
+                    //imageView가 추가될 layout
+                    imageLayout = imageLayout.findViewById(R.id.imageLayout);
+
+                    //layout param 생성
+
+                    LinearLayout.LayoutParams layoutParams =
+                            new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT *//* layout_width *//*, LinearLayout.LayoutParams.WRAP_CONTENT *//* layout_height *//*, 1f *//* layout_weight *//*);
+                    ImageView iv = new ImageView(getActivity());  // 새로 추가할 imageView 생성
+                    iv.setImageResource(R.drawable.image);  // imageView에 내용 추가
+                    iv.setLayoutParams(layoutParams);  // imageView layout 설정
+                    imageLayout.addView(iv); // 기존 linearLayout에 imageView 추가
+
+                    Glide.with(getActivity().getApplicationContext()).load(selectedImageUri).into(backgroundImage);
+                    break;
+            }
+        }
+    }*/
 
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
