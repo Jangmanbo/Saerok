0fa263b4a250cb2581451bb934336cfebf860a28
Jangmanbo <wkddpdnjs99@gmail.com>
2021-10-09 23:33:45 +0900

    Move the image registration function from MyFageFragment to SettingActivity.

--- app/src/main/java/com/example/Saerok/activity/SettingActivity.java
+++ app/src/main/java/com/example/Saerok/activity/SettingActivity.java
@@ -1,18 +1,72 @@
 package com.example.Saerok.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.Saerok.R;
 
 public class SettingActivity extends AppCompatActivity {
+    private static final int GET_IMAGE_FOR_BACKGROUNDIMAGE = 201;
+    private static final int GET_IMAGE_FOR_PROFILEIMAGE = 202;
+    private ImageView backgroundImage, profileImage;
+    private Intent intent;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        backgroundImage = findViewById(R.id.backgroundImage);
+        backgroundImage.setOnClickListener(click);
+
+        profileImage = findViewById(R.id.profileImage);
+        profileImage.setOnClickListener(click);
     }
 
+    View.OnClickListener click=new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.backgroundImage:
+                    intent = new Intent(Intent.ACTION_PICK);
+                    intent.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                    startActivityForResult(intent, GET_IMAGE_FOR_BACKGROUNDIMAGE);
+                    break;
+                case R.id.profileImage:
+                    intent = new Intent(Intent.ACTION_PICK);
+                    intent.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                    startActivityForResult(intent, GET_IMAGE_FOR_PROFILEIMAGE);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Uri selectedImageUri;
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && data != null && data.getData() != null) {
+            switch(requestCode) {
+                case GET_IMAGE_FOR_BACKGROUNDIMAGE:
+                    selectedImageUri = data.getData();
+                    Glide.with(getApplicationContext()).load(selectedImageUri).into(backgroundImage);
+                    break;
+                case GET_IMAGE_FOR_PROFILEIMAGE:
+                    selectedImageUri = data.getData();
+                    Glide.with(getApplicationContext()).load(selectedImageUri).into(profileImage);
+                    break;
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/Saerok/fragment/MyPageFragment.java
+++ app/src/main/java/com/example/Saerok/fragment/MyPageFragment.java
@@ -29,9 +29,6 @@ import java.util.ArrayList;
 import static android.app.Activity.RESULT_OK;
 
 public class MyPageFragment extends Fragment {
-    private static final int GET_IMAGE_FOR_BACKGROUNDIMAGE = 201;
-    private static final int GET_IMAGE_FOR_PROFILEIMAGE = 202;
-    private ImageView backgroundImage, profileImage;
     private ImageButton settingsButton, addCategoryButton;
     private RecyclerView recyclerView;
     private CategoryAdapter adapter;
@@ -41,25 +38,6 @@ public class MyPageFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.activity_mypage, container, false);
-        backgroundImage = view.findViewById(R.id.backgroundImage);
-        backgroundImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-                startActivityForResult(intent, GET_IMAGE_FOR_BACKGROUNDIMAGE);
-            }
-        });
-
-        profileImage = view.findViewById(R.id.profileImage);
-        profileImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-                startActivityForResult(intent, GET_IMAGE_FOR_PROFILEIMAGE);
-            }
-        });
         settingsButton = view.findViewById(R.id.settingsButton);
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,25 +86,5 @@ public class MyPageFragment extends Fragment {
 
 
         return view;
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Uri selectedImageUri;
-
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == RESULT_OK && data != null && data.getData() != null) {
-            switch(requestCode) {
-                case GET_IMAGE_FOR_BACKGROUNDIMAGE:
-                    selectedImageUri = data.getData();
-                    Glide.with(getActivity().getApplicationContext()).load(selectedImageUri).into(backgroundImage);
-                    break;
-                case GET_IMAGE_FOR_PROFILEIMAGE:
-                    selectedImageUri = data.getData();
-                    Glide.with(getActivity().getApplicationContext()).load(selectedImageUri).into(profileImage);
-                    break;
-            }
-        }
     }
 }
