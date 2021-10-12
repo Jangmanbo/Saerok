73b198e53990357549b76cb3bb6327684c70fb3c
Jangmanbo <wkddpdnjs99@gmail.com>
2021-10-12 16:06:53 +0900

    Writing the function code for the toolbar.

--- app/src/main/java/com/example/Saerok/activity/SettingActivity.java
+++ app/src/main/java/com/example/Saerok/activity/SettingActivity.java
@@ -3,11 +3,17 @@ package com.example.Saerok.activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.bumptech.glide.Glide;
 import com.example.Saerok.R;
@@ -17,6 +23,7 @@ public class SettingActivity extends AppCompatActivity {
     private static final int GET_IMAGE_FOR_PROFILEIMAGE = 202;
     private ImageView backgroundImage, profileImage;
     private Intent intent;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,6 +35,13 @@ public class SettingActivity extends AppCompatActivity {
 
         profileImage = findViewById(R.id.profileImage);
         profileImage.setOnClickListener(click);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar(); //ActionBar 객체 생성(액션바 커스텀마이징을 위해)
+
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+        actionBar.setDisplayShowTitleEnabled(false); //기본 타이틀 제거
     }
 
     View.OnClickListener click=new View.OnClickListener() {
@@ -68,5 +82,27 @@ public class SettingActivity extends AppCompatActivity {
                     break;
             }
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+
+            case R.id.store:
+                Toast.makeText(this.getApplicationContext(), "저장 버튼 클릭",Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //ToolBar에 menu.xml을 인플레이트함
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.setting_toolbar, menu);
+        return true;
     }
 }
